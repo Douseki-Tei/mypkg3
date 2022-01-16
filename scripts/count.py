b0038e9d843750023d55833507ef9479bf8f67ca
Douseki <s20C1075RH@s.chibakoudai.jp>
2022-01-16 05:57:58 +0000

    add

---
+++ scripts/count.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+#SPDX-License-Identifier:BSD-2.0
+
+#*Copyright(c)2022 Ryuich Ueda. All rights reserved.
+#*Copyright(c)2022 Douseki Tei. All rights reserved.
+
+import rospy
+from std_msgs.msg import Int32
+
+rospy.Init_node('count')
+pub = rospy.Publisher('count_up', Int32, queue_size=1)
+rate = rospy.Rate(10)
+n = 0
+while not rospy.is_shutdown():
+     n += 1
+     pub.publish(n)
+     rate.sleep()
+
