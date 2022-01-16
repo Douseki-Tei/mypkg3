c0f460ee96a6f24d954142bfe7860171994bdb78
Douseki <s20C1075RH@s.chibakoudai.jp>
2022-01-16 07:18:01 +0000

    third.py

--- scripts/third.py
+++ scripts/third.py
@@ -17,7 +17,7 @@ def cb(message):
 if __name__== '__main__':
     rospy.init_node('third')
     sub = rospy.Subscriber('count_up', Int32, cb)
-    pub = rospy.Publisher('twice', Int32, queue_size=1)
+    pub = rospy.Publisher('third', Int32, queue_size=1)
     rate = rospy.Rate(10)
     while not rospy.is_shutdown():
         pub.publish(n)
